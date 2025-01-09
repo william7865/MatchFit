f99b9f227bf807df6e7e37d5614487e2c4e0a868
Shunouu <146061752+Shunouu@users.noreply.github.com>
2025-01-09 13:26:25 +0100

    Changement

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -1,24 +1,23 @@
 <link rel="stylesheet" href="/css/header.css">
-
 <header>
-  <div class="logo">logo</div>
-  <nav>
-    <ul>
-      <li><a href="/">Accueil</a></li>
-      <?php if (isset($_SESSION['user_id'])): ?>
-        <li><a href="/coach">Coach</a></li>
-        <li><a href="/free-courses">Cours</a></li>
-        <li>
-          <form action="/logout" method="post" style="display:inline;">
-            <button type="submit" class="btn-nav">Déconnexion</button>
-          </form>
-        </li>
-      <?php else: ?>
-        <li class="buttons">
-        <a href="/login" class="btn-nav"><img src="/image/icone.png" alt="Connexion" class="icon"></a>
-        <a href="/register" class="btn-nav">Inscription</a>
-        </li>
-      <?php endif; ?>
-    </ul>
-  </nav>
+    <div class="logo">logo</div>
+    <nav>
+        <ul>
+            <li><a href="/">Accueil</a></li>
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <li><a href="/coach">Coach</a></li>
+                <li><a href="/free-courses">Cours</a></li>
+                <li>
+                    <form action="/logout" method="post" style="display:inline;">
+                        <button type="submit">Déconnexion</button>
+                    </form>
+                </li>
+            <?php else: ?>
+                <div class="buttons">
+                    <a href="/register" class="btn-nav">Inscription</a>
+                    <a href="/login" class="btn-nav">Connexion</a>
+                </div>
+            <?php endif; ?>
+        </ul>
+    </nav>
 </header>
